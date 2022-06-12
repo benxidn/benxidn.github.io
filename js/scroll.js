2bd44ca673f002b511ad383f3a5407d6177e9e4a
Benx Official <107174900+benxidn@users.noreply.github.com>
2022-06-13 00:45:40 +0700

    Create scroll.js

---
+++ js/scroll.js
@@ -0,0 +1,10 @@
+const down = document.getElementById('down');
+const up = document.getElementById('up');
+down.addEventListener('click', () => {
+  down.style.display = 'none';
+  up.style.display = 'block';
+});
+up.addEventListener('click', () => {
+  up.style.display = 'none';
+  down.style.display = 'block';
+});
