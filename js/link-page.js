8a18d1e7701c3ceabe3ace50eefb2db56d463c07
Benx Official <107174900+benxidn@users.noreply.github.com>
2022-06-30 14:43:12 +0700

    Update link-page.js

--- js/link-page.js
+++ js/link-page.js
@@ -291,7 +291,7 @@
    window.open("https://cararegistrasi.com/mzLRtlBPd0","_self");
    }
    function MainIS() {
-   window.open("https://ponselharian.com/1FPCUVg","_self");
+   window.open("https://ponselharian.com/f4pjXA","_self");
    }
    
    /*⭐⭐⭐
