b1a002e0d1f533d5055e70ccc604302a75ef59cf
Benx Official <107174900+benxidn@users.noreply.github.com>
2022-07-07 00:27:57 +0700

    Update link-page.js

--- js/link-page.js
+++ js/link-page.js
@@ -1,319 +1,241 @@
-/*•• MAIN LINK ••*/
-   function MainDS() { /*DV SC 32bit*/
-   window.open("https://ponselharian.com/Ql2Wi","_self"); }
-   function MainL1() { /*Sc DV 1X 32bit*/
-   window.open("https://cararegistrasi.com/F54P","_self"); }
-   function MainL2() { /*Sc DV 2X 32bit*/
-   window.open("https://cararegistrasi.com/umQ4gy","_self"); }
-   function MainL3() { /*Sc DV 3X 32bit*/
-   window.open("https://cararegistrasi.com/jiyg","_self"); }
-   function MainL4() { /*Sc DV 4X 32bit*/
-   window.open("https://cararegistrasi.com/qVqoNzKlgeb2","_self"); }
-   function MainL5() { /*Sc DV 5X 32bit*/
-   window.open("https://cararegistrasi.com/JxDL7IAD","_self"); }
-   function MainL6() { /*Sc DV 6X 32bit*/
-   window.open("https://cararegistrasi.com/qqCHb6O6kvH","_self"); }
-   function MainL7() { /*Sc DV 7X 32bit*/
-   window.open("https://cararegistrasi.com/T8VKpTnxe","_self"); }
-   
-   function MainDAS() { /*DV APK 32bit*/
-   window.open("https://ponselharian.com/mQj0","_self"); }
-   function MainDA() { /*DV APK 64bit*/
-   window.open("https://ponselharian.com/ydVR","_self"); }
-   
-   function MainLN() { /*LBX MU*/
-   window.open("https://ponselharian.com/tpTf","_self"); }
-   function MainSFr2() { /*Sc LMU 2X*/
-   window.open("https://cararegistrasi.com/nOtZxA3","_self"); }
-   function MainSFr3() { /*Sc LMU 3X*/
-   window.open("https://cararegistrasi.com/ZMUp0KW9ii","_self"); }
-   function MainSFr4() { /*Sc LMU 4X*/
-   window.open("https://cararegistrasi.com/u07ao","_self"); }
-   function MainSFr5() { /*Sc LMU 5X*/
-   window.open("https://cararegistrasi.com/uNG6iWa","_self"); }
-   function MainSFr6() { /*Sc LMU 6X*/
-   window.open("https://cararegistrasi.com/nmuivok","_self"); }
-   function MainSFr7() { /*Sc LMU 7X*/
-   window.open("https://cararegistrasi.com/Dyfcp92","_self"); }
-   function MainSFrBu() { /*Sc LMU BU*/
-   window.open("https://cararegistrasi.com/xm3NbrDFe0t","_self"); }
-   
-   function MainLAu() { /*LBX AU*/
-   window.open("https://ponselharian.com/MFXK","_self"); }
-   /*Key LBX AU*/
-   function MainS1() { /*Sc LAU 1X*/
-   window.open("https://semawur.com/xFF3KPNz","_self"); }
-   function MainS2() { /*Sc LAU 2X*/
-   window.open("https://cararegistrasi.com/JQod","_self"); }
-   function MainS3() { /*Sc LAU 3X*/
-   window.open("https://cararegistrasi.com/G67lp","_self"); }
-   function MainS4() { /*Sc LAU 4X*/
-   window.open("https://cararegistrasi.com/8niZ1MbPC4","_self"); }
-   function MainS5() { /*Sc LAU 5X*/
-   window.open("https://cararegistrasi.com/YwTYqa","_self"); }
-   function MainBU() { /*Sc LAU BU*/
-   window.open("https://cararegistrasi.com/9UPml3k","_self"); }
-   
-   function MainMAS() { /*MH AA SKIN*/
-   window.open("https://ponselharian.com/MxEBfQTQ","_self"); }
-   function MainMA() { /*MH AA*/
-   window.open("https://ponselharian.com/0MOGP96u9UFd","_self"); }
-   
-   function MainMK() { /*MH KN*/
-   window.open("https://ponselharian.com/sbKZPhHM","_self"); }
-   
-   function MainMF() { /*MH FC*/
-   window.open("https://ponselharian.com/LA8MvB","_self"); }
-   /*Key MH FC*/
-   
-   function MainIS() { /*UAS APK*/
-   window.open("https://ponselharian.com/1mJ7XUfon","_self"); }
-   
-   function MainRA() { /*RB APK*/
-   window.open("https://ponselharian.com/YkNZuD","_self"); }
-   
-   function MainRS() { /*RB SC*/
-   window.open("https://ponselharian.com/2cEix3","_self"); }
-   
-   function MainDuS() { /*DU SC*/
-   window.open("https://ponselharian.com/I3YusiVKIFL","_self"); }
-   
-   function MainHS() { /*HOOK SC*/
-   window.open("https://ponselharian.com/QuYMQp","_self"); }
-   
-/*•• MORE LINK ••*/
-   function ZArchiver() {
-   window.open("https://ponselharian.com/xRFvrfw8CE"); }
-   function MTManager() {
-   window.open("https://ponselharian.com/toNp7"); }
-   function Brave() {
-   window.open("https://ponselharian.com/nnhAB"); }
-   
-   /*⭐⭐⭐
-   MORE LINK*/
-   function MoreKFc() {
-   window.open("https://ponselharian.com/0aOycce");
-   }
-   function MoreKLb() {
-   window.open("https://ponselharian.com/Muk1vnBum");
-   }
-   
-   /*⭐⭐⭐
-   MAPHACK AA*/
-   function MhAaSkin() {
-   window.open("https://ponselharian.com/lRvY5sN9wg");
-   }
-   function SubMAS() {
-   window.open("https://cararegistrasi.com/6DI142MLg","_self");
-   }
-   
-   function MhAa() {
-   window.open("https://ponselharian.com/TKti");
-   }
-   function SubMA() {
-   window.open("https://cararegistrasi.com/UYfeD77zWDvu","_self");
-   }
-   /*
-   function FtrMhAa() {
-   window.open("https://t.me/segawonchannel/215","_self");
-   }*/
-   
-   /*⭐⭐⭐
-   MAPHACK KN*/
-   function MhKn() {
-   window.open("https://ponselharian.com/xV8a6G8LX2");
-   }
-   function SubMK() {
-   window.open("https://cararegistrasi.com/EvOfOy3Lcn0","_self");
-   }/*
-   function FtrMhKn() {
-   window.open("https://t.me/segawonchannel/217","_self");
-   }*/
-   
-   /*⭐⭐⭐
-   MAPHACK FC*/
-   function MhFc() {
-   window.open("https://ponselharian.com/M2DNtoZJ");
-   }
-   function SubMF() {
-   window.open("https://cararegistrasi.com/AvIwkCNss","_self");
-   }
-   
-   function KeyFc() {
-   window.open("https://ponselharian.com/S6SrER");
-   }
-   /*
-   function FtrMhFc() {
-   window.open("https://t.me/segawonchannel/219","_self");
-   }*/
-   
-   /*⭐⭐⭐
-   DV APK*/
-   function DvApkSkin() {
-   window.open("https://ponselharian.com/4d21");
-   }
-   function SubDAS() {
-   window.open("https://cararegistrasi.com/wjPyDa","_self");
-   }
-   
-   function DvApk() {
-   window.open("https://ponselharian.com/xzrE0QGi");
-   }
-   function SubDA() {
-   window.open("https://cararegistrasi.com/DsAELZ4O","_self");
-   }
-   
-   /*⭐⭐⭐
-   DV SC*/
-   function DvSc() {
-   window.open("https://ponselharian.com/nHNKtR5kG");
-   }
-   function SubDS() {
-   window.open("https://cararegistrasi.com/z5likCtCvF","_self");
-   }
-   
-   function Lib1() {
-   window.open("https://ponselharian.com/0hkk66vFZ");
-   }
-   
-   function Lib2() {
-   window.open("https://ponselharian.com/VRxWQUj");
-   }
-   
-   function Lib3() {
-   window.open("https://ponselharian.com/25UYvOeemAs");
-   }
-   
-   function Lib4() {
-   window.open("https://ponselharian.com/sYhpgICs");
-   }
-   
-   function Lib5() {
-   window.open("https://ponselharian.com/ubCpBsCZT");
-   }
-   
-   function Lib6() {
-   window.open("https://ponselharian.com/QaH6");
-   }
-   
-   function Lib7() {
-   window.open("https://ponselharian.com/nxJJUgM");
-   }
-   
-   /*⭐⭐⭐
-   LULUBOX AU*/
-   function LbAu() {
-   window.open("https://ponselharian.com/0VJ");
-   }
-   function SubLAu() {
-   window.open("https://cararegistrasi.com/0H7cBZfC0","_self");
-   }
-   
-   function Sc1() {
-   window.open("https://ponselharian.com/8LECWnm6GVJx");
-   }
-   
-   function Sc2() {
-   window.open("https://ponselharian.com/uAw");
-   }
-   
-   function Sc3() {
-   window.open("https://ponselharian.com/fkL6rmJFiQO");
-   }
-   
-   function Sc4() {
-   window.open("https://ponselharian.com/UgV6k");
-   }
-   
-   function Sc5() {
-   window.open("https://ponselharian.com/LXmmLLA4glKC");
-   }
-   
-   function ScBu() {
-   window.open("https://ponselharian.com/M2fbH");
-   }
-   
-   /*⭐⭐⭐
-   LULUBOX NK*/
-   function LbNk() {
-   window.open("https://ponselharian.com/tB6TmHRDg1F");
-   }
-   function SubLN() {
-   window.open("https://cararegistrasi.com/YRHJjplThsYb","_self");
-   }
-   
-   function SFr2() {
-   window.open("https://ponselharian.com/uHhbPn3OO");
-   }
-   
-   function SFr3() {
-   window.open("https://ponselharian.com/Opsu");
-   }
-   
-   function SFr4() {
-   window.open("https://ponselharian.com/6uh");
-   }
-   
-   function SFr5() {
-   window.open("https://ponselharian.com/IF9gXkA");
-   }
-   
-   function SFr6() {
-   window.open("https://ponselharian.com/IQQU0ZgRUJXj");
-   }
-   
-   function SFr7() {
-   window.open("https://ponselharian.com/nPBStcqTj");
-   }
-   
-   function SFrBu() {
-   window.open("https://ponselharian.com/pASGD");
-   }
-   
-   /*⭐⭐⭐
-   INJECT SKIN*/
-   function Injt() {
-   window.open("https://ponselharian.com/ErxSVVf0");
-   }
-   function SubIS() {
-   window.open("https://cararegistrasi.com/mzLRtlBPd0","_self");
-   }
-   
-   /*⭐⭐⭐
-   BOOSTER APK*/
-   function RbApk() {
-   window.open("https://ponselharian.com/TS8pPpuL");
-   }
-   function SubRA() {
-   window.open("https://cararegistrasi.com/kpPbT1pR","_self");
-   }
-   
-   /*⭐⭐⭐
-   BOOSTER SC*/
-   function RbSc() {
-   window.open("https://ponselharian.com/KJwzBiqJq3");
-   }
-   function SubRS() {
-   window.open("https://cararegistrasi.com/N9YSnN","_self");
-   }
-   
-   /*⭐⭐⭐
-   DAMAGE SC*/
-   function DuSc() {
-   window.open("https://ponselharian.com/HjV7S");
-   }
-   function SubDuS() {
-   window.open("https://cararegistrasi.com/RoZfIpc1DUaL","_self");
-   }
-   
-   /*⭐⭐⭐
-   HOOK SC*/
-   function HokSc() {
-   window.open("https://ponselharian.com/H9FBqVXycG");
-   }
-   function SubHS() {
-   window.open("https://cararegistrasi.com/0lvWlljWA","_self");
-   }
-   function LinkHokApk() { 
-   window.open("mh-kn.html");
-   }
+/* ### MAIN LINK ### */
+/*DV SC 32bit*/
+function MainDS() { window.open("https://ponselharian.com/Ql2Wi","_self"); }
+/*Sc DV 1X 32bit*/
+function MainL1() { window.open("https://cararegistrasi.com/F54P","_self"); }
+/*Sc DV 2X 32bit*/
+function MainL2() { window.open("https://cararegistrasi.com/umQ4gy","_self"); }
+/*Sc DV 3X 32bit*/
+function MainL3() { window.open("https://cararegistrasi.com/jiyg","_self"); }
+/*Sc DV 4X 32bit*/
+function MainL4() { window.open("https://cararegistrasi.com/qVqoNzKlgeb2","_self"); }
+/*Sc DV 5X 32bit*/
+function MainL5() { window.open("https://cararegistrasi.com/JxDL7IAD","_self"); }
+/*Sc DV 6X 32bit*/
+function MainL6() { window.open("https://cararegistrasi.com/qqCHb6O6kvH","_self"); }
+/*Sc DV 7X 32bit*/
+function MainL7() { window.open("https://cararegistrasi.com/T8VKpTnxe","_self"); }
+
+/*DV APK 32bit*/
+function MainDAS() { window.open("https://ponselharian.com/mQj0","_self"); }
+/*DV APK 64bit*/
+function MainDA() { window.open("https://ponselharian.com/ydVR","_self"); }
+
+/*LBX MU*/
+function MainLN() { window.open("https://ponselharian.com/tpTf","_self"); }
+/*Sc LMU 2X*/
+function MainSFr2() { window.open("https://cararegistrasi.com/nOtZxA3","_self"); }
+/*Sc LMU 3X*/
+function MainSFr3() { window.open("https://cararegistrasi.com/ZMUp0KW9ii","_self"); }
+/*Sc LMU 4X*/
+function MainSFr4() { window.open("https://cararegistrasi.com/u07ao","_self"); }
+/*Sc LMU 5X*/
+function MainSFr5() { window.open("https://cararegistrasi.com/uNG6iWa","_self"); }
+/*Sc LMU 6X*/
+function MainSFr6() { window.open("https://cararegistrasi.com/nmuivok","_self"); }
+/*Sc LMU 7X*/
+function MainSFr7() { window.open("https://cararegistrasi.com/Dyfcp92","_self"); }
+/*Sc LMU BU*/
+function MainSFrBu() { window.open("https://cararegistrasi.com/xm3NbrDFe0t","_self"); }
+
+/*LBX AU*/
+function MainLAu() { window.open("https://ponselharian.com/MFXK","_self"); }
+/*Key LBX AU*/
+/*Sc LAU 1X*/
+function MainS1() { window.open("https://semawur.com/xFF3KPNz","_self"); }
+/*Sc LAU 2X*/
+function MainS2() { window.open("https://cararegistrasi.com/JQod","_self"); }
+/*Sc LAU 3X*/
+function MainS3() { window.open("https://cararegistrasi.com/G67lp","_self"); }
+/*Sc LAU 4X*/
+function MainS4() { window.open("https://cararegistrasi.com/8niZ1MbPC4","_self"); }
+/*Sc LAU 5X*/
+function MainS5() { window.open("https://cararegistrasi.com/YwTYqa","_self"); }
+/*Sc LAU BU*/
+function MainBU() { window.open("https://cararegistrasi.com/9UPml3k","_self"); }
+
+/*MH AA SKIN*/
+function MainMAS() { window.open("https://ponselharian.com/MxEBfQTQ","_self"); }
+/*MH AA*/
+function MainMA() { window.open("https://ponselharian.com/0MOGP96u9UFd","_self"); }
+
+/*MH KN*/
+function MainMK() { window.open("https://ponselharian.com/sbKZPhHM","_self"); }
+
+/*MH FC*/
+function MainMF() { window.open("https://ponselharian.com/LA8MvB","_self"); }
+/*Key MH FC*/
+
+/*UAS APK*/
+function MainIS() { window.open("https://ponselharian.com/1mJ7XUfon","_self"); }
+
+/*RB APK*/
+function MainRA() { window.open("https://ponselharian.com/YkNZuD","_self"); }
+
+/*RB SC*/
+function MainRS() { window.open("https://ponselharian.com/2cEix3","_self"); }
+
+/*DU SC*/
+function MainDuS() { window.open("https://ponselharian.com/I3YusiVKIFL","_self"); }
+
+/*HOOK SC*/
+function MainHS() { window.open("https://ponselharian.com/QuYMQp","_self"); }
+
+/*MORE LINK*/
+function ZArchiver() { window.open("https://ponselharian.com/xRFvrfw8CE"); }
+function MTManager() { window.open("https://ponselharian.com/toNp7"); }
+function Brave() { window.open("https://ponselharian.com/nnhAB"); }
+
+/* ########## */
+
+/* ### MORE LINK ### */
+function MoreKFc() {
+  window.open("https://ponselharian.com/0aOycce"); }
+function MoreKLb() {
+  window.open("https://ponselharian.com/Muk1vnBum"); }
+
+/* ### MAPHACK AA ### */
+function MhAaSkin() {
+  window.open("https://ponselharian.com/lRvY5sN9wg"); }
+function SubMAS() {
+  window.open("https://cararegistrasi.com/6DI142MLg","_self"); }
+
+function MhAa() {
+  window.open("https://ponselharian.com/TKti"); }
+function SubMA() {
+  window.open("https://cararegistrasi.com/UYfeD77zWDvu","_self"); }
+
+/* ### MAPHACK KN ### */
+function MhKn() {
+  window.open("https://ponselharian.com/xV8a6G8LX2"); }
+function SubMK() {
+  window.open("https://cararegistrasi.com/EvOfOy3Lcn0","_self"); }
+
+/* ### MAPHACK FC ### */
+function MhFc() {
+  window.open("https://ponselharian.com/M2DNtoZJ"); }
+function SubMF() {
+  window.open("https://cararegistrasi.com/AvIwkCNss","_self"); }
+
+function KeyFc() {
+  window.open("https://ponselharian.com/S6SrER"); }
+
+/* ### DV APK ### */
+function DvApkSkin() {
+  window.open("https://ponselharian.com/4d21"); }
+function SubDAS() {
+  window.open("https://cararegistrasi.com/wjPyDa","_self"); }
+
+function DvApk() {
+  window.open("https://ponselharian.com/xzrE0QGi"); }
+function SubDA() {
+  window.open("https://cararegistrasi.com/DsAELZ4O","_self"); }
+
+/* ### DV SC ### */
+function DvSc() {
+  window.open("https://ponselharian.com/nHNKtR5kG"); }
+function SubDS() {
+  window.open("https://cararegistrasi.com/z5likCtCvF","_self"); }
+
+function Lib1() {
+  window.open("https://ponselharian.com/0hkk66vFZ"); }
+
+function Lib2() {
+  window.open("https://ponselharian.com/VRxWQUj"); }
+
+function Lib3() {
+  window.open("https://ponselharian.com/25UYvOeemAs"); }
+
+function Lib4() {
+  window.open("https://ponselharian.com/sYhpgICs"); }
+
+function Lib5() {
+  window.open("https://ponselharian.com/ubCpBsCZT"); }
+
+function Lib6() {
+  window.open("https://ponselharian.com/QaH6"); }
+
+function Lib7() {
+  window.open("https://ponselharian.com/nxJJUgM"); }
+
+/* ### LULUBOX AU ### */
+function LbAu() {
+  window.open("https://ponselharian.com/0VJ"); }
+function SubLAu() {
+  window.open("https://cararegistrasi.com/0H7cBZfC0","_self"); }
+
+function Sc1() {
+  window.open("https://ponselharian.com/8LECWnm6GVJx"); }
+
+function Sc2() {
+  window.open("https://ponselharian.com/uAw"); }
+
+function Sc3() {
+  window.open("https://ponselharian.com/fkL6rmJFiQO"); }
+
+function Sc4() {
+  window.open("https://ponselharian.com/UgV6k"); }
+
+function Sc5() {
+  window.open("https://ponselharian.com/LXmmLLA4glKC"); }
+
+function ScBu() {
+  window.open("https://ponselharian.com/M2fbH"); }
+
+/* ### LULUBOX NK ### */
+function LbNk() {
+  window.open("https://ponselharian.com/tB6TmHRDg1F"); }
+function SubLN() {
+  window.open("https://cararegistrasi.com/YRHJjplThsYb","_self"); }
+
+function SFr2() {
+  window.open("https://ponselharian.com/uHhbPn3OO"); }
+
+function SFr3() {
+  window.open("https://ponselharian.com/Opsu"); }
+
+function SFr4() {
+  window.open("https://ponselharian.com/6uh"); }
+
+function SFr5() {
+  window.open("https://ponselharian.com/IF9gXkA"); }
+
+function SFr6() {
+  window.open("https://ponselharian.com/IQQU0ZgRUJXj"); }
+
+function SFr7() {
+  window.open("https://ponselharian.com/nPBStcqTj"); }
+
+function SFrBu() {
+  window.open("https://ponselharian.com/pASGD"); }
+
+/* ### INJECT SKIN ### */
+function Injt() {
+  window.open("https://ponselharian.com/ErxSVVf0"); }
+function SubIS() {
+  window.open("https://cararegistrasi.com/mzLRtlBPd0","_self"); }
+
+/* ### BOOSTER APK ### */
+function RbApk() {
+  window.open("https://ponselharian.com/TS8pPpuL"); }
+function SubRA() {
+  window.open("https://cararegistrasi.com/kpPbT1pR","_self"); }
+/* ### BOOSTER SC ### */
+function RbSc() {
+  window.open("https://ponselharian.com/KJwzBiqJq3"); }
+function SubRS() {
+  window.open("https://cararegistrasi.com/N9YSnN","_self"); }
+
+/* ### DAMAGE SC ### */
+function DuSc() {
+  window.open("https://ponselharian.com/HjV7S"); }
+function SubDuS() {
+  window.open("https://cararegistrasi.com/RoZfIpc1DUaL","_self"); }
+
+/* ### HOOK SC ### */
+function HokSc() {
+  window.open("https://ponselharian.com/H9FBqVXycG"); }
+function SubHS() {
+  window.open("https://cararegistrasi.com/0lvWlljWA","_self"); }
+function LinkHokApk() { 
+  window.open("mh-kn.html"); }
