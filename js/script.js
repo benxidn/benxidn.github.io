b8b6800e19ebdb29be07b9be71be1c41194fadb1
Benx Official <107174900+benxidn@users.noreply.github.com>
2025-04-20 02:57:11 +0700

    Update Files

---
+++ js/script.js
@@ -0,0 +1,41 @@
+function Home() {
+    window.open("../../","_self");
+    }
+ function Channel() {
+    window.open("https://dexter.rf.gd/1/channel1","_self");
+    }
+ function AntiBan() {
+    window.open("https://youtu.be/szocEpRmGTI","_blank");
+    }
+ function Download() {
+    window.open("https://youtu.be/Hl3Gu1IejBg","_blank");
+    }
+ function InstallAPK() {
+    window.open("https://youtu.be/gyDQEhZl4es","_blank");
+    }
+ function AddScript() {
+    window.open("https://youtu.be/6aqGQwdazVg","_blank");
+    }
+ function Server5K() {
+    window.open("https://youtu.be/EoyTJI0PyRU","_blank");
+    }
+ function BeliAkun() {
+    window.open("https://wa.me/6281338214136?text=Halo+min+saya+mau+beli+AKUN+ML+SERVER+5K+harga+5K+!!!","_blank");
+    }
+ function Drone() {
+    window.open("https://dexter.rf.gd/1/channel1","_self");
+    }
+ function Fakecez() {
+    window.open("https://dexter.rf.gd/1/channel1","_self");
+    }
+ function Dexter() {
+    window.open("https://dexter.rf.gd/1/channel1","_self");
+    }
+ let stringText = '000';
+ function copyBtn() {
+    navigator.clipboard.writeText(stringText);
+    }
+ let stringText2  = 'SUBSCRIBE';
+ function copyBtn2() {
+    navigator.clipboard.writeText(stringText2);
+    }
