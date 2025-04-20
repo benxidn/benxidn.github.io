f170919c7d5ec260febe67f406da93e3363ebcda
Benx Official <107174900+benxidn@users.noreply.github.com>
2025-04-20 11:39:10 +0700

    p

--- js/script.js
+++ js/script.js
@@ -1,30 +1,3 @@
-//   // Menyisipkan file html secara dinamis
-//   fetch('components/header.html')
-//     .then(response => response.text())
-//     .then(data => {
-//       document.getElementById("header").innerHTML = data;
-//     })
-//   fetch('components/footer.html')
-//     .then(response => response.text())
-//     .then(data => {
-//       document.getElementById("footer").innerHTML = data;
-//     })
-//     .catch(error => {
-//       console.error("Gagal memuat konten:", error);
-//     });
-
-//   // Setelah halaman dimuat, tunggu hingga elemen #tahun muncul
-//       document.addEventListener("DOMContentLoaded", () => {
-//         const tahunSekarang = new Date().getFullYear();
-//         const cekTahun = setInterval(() => {
-//           const el = document.getElementById("tahun");
-//           if (el) {
-//             el.textContent = tahunSekarang;
-//             clearInterval(cekTahun);
-//           }
-//         }, 100);
-//       });
-
 function loadComponent(id, path) {
    fetch(path)
      .then(response => response.text())
@@ -40,7 +13,8 @@ function loadComponent(id, path) {
    loadComponent("header", "components/header.html");
    loadComponent("footer", "components/footer.html");
  
-   // Menyisipkan tahun saat ini
+// Menyisipkan tahun saat ini ke elemen dengan id "tahun"
+document.addEventListener("DOMContentLoaded", () => {
    const tahunSekarang = new Date().getFullYear();
    const cekTahun = setInterval(() => {
      const el = document.getElementById("tahun");
@@ -50,44 +24,57 @@ function loadComponent(id, path) {
      }
    }, 100);
  });
- 
 
-// Fungsi buka link
+// Fungsi navigasi ke berbagai link
 function Home() {
-    window.open("../../../","_self");
-    }
+   window.open("../../../", "_self");
+ }
+ 
  function Channel() {
-    window.open("https://dexter.rf.gd/1/channel1","_self");
-    }
+   window.open("https://dexter.rf.gd/1/channel1", "_self");
+ }
+ 
  function AntiBan() {
-    window.open("https://youtu.be/szocEpRmGTI","_blank");
-    }
+   window.open("https://youtu.be/szocEpRmGTI", "_blank");
+ }
+ 
  function Download() {
-    window.open("https://youtu.be/Hl3Gu1IejBg","_blank");
-    }
+   window.open("https://youtu.be/Hl3Gu1IejBg", "_blank");
+ }
+ 
  function InstallAPK() {
-    window.open("https://youtu.be/gyDQEhZl4es","_blank");
-    }
+   window.open("https://youtu.be/gyDQEhZl4es", "_blank");
+ }
+ 
  function AddScript() {
-    window.open("https://youtu.be/6aqGQwdazVg","_blank");
-    }
+   window.open("https://youtu.be/6aqGQwdazVg", "_blank");
+ }
+ 
  function Server5K() {
-    window.open("https://youtu.be/EoyTJI0PyRU","_blank");
-    }
+   window.open("https://youtu.be/EoyTJI0PyRU", "_blank");
+ }
+ 
  function BeliAkun() {
-    window.open("https://wa.me/6281338214136?text=Halo+min+saya+mau+beli+AKUN+ML+SERVER+5K+harga+5K+!!!","_blank");
-    }
+   window.open("https://wa.me/6281338214136?text=Halo+min+saya+mau+beli+AKUN+ML+SERVER+5K+harga+5K+!!!", "_blank");
+ }
+ 
  function Drone() {
-    window.open("https://dexter.rf.gd/1/channel1","_self");
-    }
+   window.open("https://dexter.rf.gd/1/channel1", "_self");
+ }
+ 
  function Fakecez() {
-    window.open("https://dexter.rf.gd/1/channel1","_self");
-    }
+   window.open("https://dexter.rf.gd/1/channel1", "_self");
+ }
+ 
  function Dexter() {
-    window.open("https://dexter.rf.gd/1/channel1","_self");
-    }
+   window.open("https://dexter.rf.gd/1/channel1", "_self");
+ }
+ 
+ // Fungsi salin teks
  let stringText = '000';
+ let stringText2 = 'SUBSCRIBE';
+ 
  function copyBtn() {
-    navigator.clipboard.writeText(stringText);
-    }
- let stringText2  = 'SUBSCRIBE';
+   navigator.clipboard.writeText(stringText);
+ }
+ 
