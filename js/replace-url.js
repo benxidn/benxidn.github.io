dc8655917e5d6a7e50f12c94b6f071462f333254
Benx Official <107174900+benxidn@users.noreply.github.com>
2025-04-20 13:48:44 +0700

    p

--- js/replace-url.js
+++ js/replace-url.js
@@ -1,3 +1,4 @@
+// Mengganti URL di address bar menjadi '/' setelah halaman selesai dimuat, tanpa memuat ulang halaman
 document.addEventListener('DOMContentLoaded', () => {
     window.history.replaceState(null, '', '/');
   });
