efe6e6bbe007416e87eaa524b63a73ac9ee816ed
Benx Official <107174900+benxidn@users.noreply.github.com>
2025-04-20 11:56:23 +0700

    p

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 // Menyisipkan file HTML secara dinamis ke dalam elemen header dan footer
-fetch('components/header.html')
+fetch('../../../components/header.html')
   .then(response => response.text())
   .then(data => {
     document.getElementById("header").innerHTML = data;
   });
 
-fetch('components/footer.html')
+fetch('../../../components/footer.html')
   .then(response => response.text())
   .then(data => {
     document.getElementById("footer").innerHTML = data;
